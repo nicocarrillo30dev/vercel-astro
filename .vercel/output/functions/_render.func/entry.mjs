bfb44002dd87438a7ead2dc5ec0d41a90e3a87af
Nicolás Carrillo <nico.carrillodev@icloud.com>
2025-01-13 17:41:36 -0500

    new changes

---
+++ .vercel/output/functions/_render.func/entry.mjs
@@ -0,0 +1,32 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_DtHt2oZ6.mjs';
+import { manifest } from './manifest_DmOA9Ar2.mjs';
+
+const serverIslandMap = new Map([
+	['BookScore', () => import('./chunks/BookScore_BxhtNcxo.mjs')],
+	['BuyButton', () => import('./chunks/BuyButton_sfQLJXN3.mjs')],
+]);;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/libro/_id_.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/.pnpm/astro@5.1.0_jiti@1.21.7_rollup@4.30.1_typescript@5.7.3_yaml@2.7.0/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/libro/[id].astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "ba358399-e04d-4244-b60a-d5fe56356e95",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,28 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   integrations: [tailwind()],
 
   env: {
     schema: {
-      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
-    }
+      SHOW_BUY_BUTTON: envField.boolean({
+        default: true,
+        context: "server",
+        access: "public",
+      }),
+      SCORE_API_ENDPOINT: envField.string({
+        context: "server",
+        access: "public",
+      }),
+    },
   },
 
-  adapter: vercel()
+  adapter: vercel(),
 });
