cf3088d71f9ace7e9ef16bc6efb0ab47612905b2
Nicolás Carrillo <nico.carrillodev@icloud.com>
2025-01-13 20:25:27 -0500

    new changes

--- .vercel/output/functions/_render.func/entry.mjs
+++ .vercel/output/functions/_render.func/entry.mjs
@@ -1,10 +1,10 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_nBNXHm_K.mjs';
-import { manifest } from './manifest_DMir5v6a.mjs';
+import { manifest } from './manifest_CXxjBDDY.mjs';
 
 const serverIslandMap = new Map([
 	['BookScore', () => import('./chunks/BookScore_GIW3XBPs.mjs')],
-	['BuyButton', () => import('./chunks/BuyButton_D91-IJW3.mjs')],
+	['BuyButton', () => import('./chunks/BuyButton_BpNRPjL4.mjs')],
 ]);;
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "b8125848-32f5-4da7-9315-98cea263cb47",
+    "middlewareSecret": "94d74cee-46d6-41b3-a594-0becb8fad8d6",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
